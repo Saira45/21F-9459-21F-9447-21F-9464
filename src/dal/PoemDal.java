2de9aa4108600cbaa92e014e021406c22dc23496
aqsajavai10 <145002052+aqsajavai10@users.noreply.github.com>
2023-11-03 16:46:06 +0500

    Insert Poem

--- src/dal/PoemDal.java
+++ src/dal/PoemDal.java
@@ -1,9 +1,50 @@
 package dal;
+import TransferObject.PoemTO;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
 public class PoemDal {
 
-	public PoemDal() {
-		// TODO Auto-generated constructor stub
-	}
+	private static final String URL = "jdbc:mysql://localhost:3306/poems ";
+    private static final String USERNAME = "root";
+    private static final String PASSWORD = "";
+
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(URL, USERNAME, PASSWORD);
+    }
+    public static void close(Connection connection, PreparedStatement preparedStatement) {
+        try {
+            if (preparedStatement != null) {
+                preparedStatement.close();
+            }
+            if (connection != null) {
+                connection.close();
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+	 public void savePoem(PoemTO poem) throws SQLException {
+	        Connection connection = null;
+	        PreparedStatement preparedStatement = null;
+
+	        
+	        
+	        try {
+	            connection = getConnection();
+	            String sql = "INSERT INTO poems1 (title, verse_number, misra1, misra2) VALUES (?, ?, ?, ?)";
+	            preparedStatement = connection.prepareStatement(sql);
+	            preparedStatement.setString(1, poem.getTitle());
+	            preparedStatement.setInt(2, poem.getVerseNumber());
+	            preparedStatement.setString(3, poem.getMisra1());
+	            preparedStatement.setString(4, poem.getMisra2());
+	            
+	            preparedStatement.executeUpdate();
+	        } finally {
+	            close(connection, preparedStatement);
+	        }
+	    }
 
 }
--- src/pl/presentationLayer.java
+++ src/pl/presentationLayer.java
@@ -1,38 +1,38 @@
 package pl;
 
 import javax.swing.*;
-
-import TransferObject.PoemTO;
-
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.sql.SQLException;
 import dal.PoemDal;
-import dal.RootDal;
-import dal.VerseDal;
+import TransferObject.PoemTO;
+
 public class presentationLayer {
     private JFrame frame;
-  
+    private JTextField titleField;
+    private JTextField verseNumberField;
     private JTextField misra1Field;
     private JTextField misra2Field;
- 
+    //private JTextField bookIdField;
 
     public presentationLayer() {
-        frame = new JFrame("Poem Database");
+        frame = new JFrame("Arabic Poem Database");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(400, 200);
-        frame.setLayout(new GridLayout(6, 3));
+        frame.setLayout(new GridLayout(5, 2));
 
-      
-       
+        JLabel titleLabel = new JLabel("Title:");
+        titleField = new JTextField();
+        JLabel verseNumberLabel = new JLabel("Verse Number:");
+        verseNumberField = new JTextField();
         JLabel misra1Label = new JLabel("Misra 1:");
         misra1Field = new JTextField();
         JLabel misra2Label = new JLabel("Misra 2:");
         misra2Field = new JTextField();
-     
+        
 
-        JButton insertButton = new JButton("Insert Verse");
+        JButton insertButton = new JButton("Insert Poem");
         insertButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -40,8 +40,11 @@ public class presentationLayer {
             }
         });
 
-
-      
+        
+        frame.add(titleLabel);
+        frame.add(titleField);
+        frame.add(verseNumberLabel);
+        frame.add(verseNumberField);
         frame.add(misra1Label);
         frame.add(misra1Field);
         frame.add(misra2Label);
@@ -52,22 +55,23 @@ public class presentationLayer {
     }
 
     private void insertPoem() {
-      
-      
+        String title = titleField.getText();
+        int verseNumber = Integer.parseInt(verseNumberField.getText());
         String misra1 = misra1Field.getText();
         String misra2 = misra2Field.getText();
-      
+        
 
         PoemTO poem = new PoemTO();
-     
+        poem.setTitle(title);
+        poem.setVerseNumber(verseNumber);
         poem.setMisra1(misra1);
         poem.setMisra2(misra2);
-     
+        
 
-       
+        PoemDal poemDAO = new PoemDal();
 
         try {
-            PoemDal.savePoem(poem);
+            poemDAO.savePoem(poem);
             JOptionPane.showMessageDialog(frame, "Poem inserted successfully.", "Success", JOptionPane.INFORMATION_MESSAGE);
             clearFields();
         } catch (SQLException ex) {
@@ -77,7 +81,8 @@ public class presentationLayer {
     }
 
     private void clearFields() {
-     
+        titleField.setText("");
+        verseNumberField.setText("");
         misra1Field.setText("");
         misra2Field.setText("");
     }
