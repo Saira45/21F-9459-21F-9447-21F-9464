23c3585787a6c12fbb2756fca516c43b1be62ca0
Nabia Zeenat Zia <137612224+Nabia-Zeenat-Zia@users.noreply.github.com>
2023-11-03 20:13:11 +0500

    Tokenization of Verse done the tokens appears as clickable buttons which when click enables roots button

--- src/bll/PoemBL.java
+++ src/bll/PoemBL.java
@@ -1,9 +1,19 @@
 package bll;
+import dal.PoemDal;
+import TransferObject.PoemTO;
+import java.sql.SQLException;
 
 public class PoemBL {
 
+	private PoemDal poemDAO;
 	public PoemBL() {
 		// TODO Auto-generated constructor stub
+		this.poemDAO = new PoemDal();
 	}
+	
+
+    public void savePoem(PoemTO poem) throws SQLException {
+        poemDAO.savePoem(poem);
+    }
 
 }
--- src/pl/presentationLayer.java
+++ src/pl/presentationLayer.java
@@ -1,88 +1,153 @@
 package pl;
 
 import javax.swing.*;
-
-import TransferObject.PoemTO;
-
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.sql.SQLException;
 import dal.PoemDal;
-import dal.RootDal;
-import dal.VerseDal;
+import TransferObject.PoemTO;
+
+import java.util.StringTokenizer;
+import java.util.ArrayList;
+
 public class presentationLayer {
-    private JFrame frame;
-  
-    private JTextField misra1Field;
-    private JTextField misra2Field;
- 
+	private JFrame frame;
+	private JTextField titleField;
+	private JTextField verseNumberField;
+	private JTextField misra1Field;
 
-    public presentationLayer() {
-        frame = new JFrame("Poem Database");
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(400, 200);
-        frame.setLayout(new GridLayout(6, 3));
+	private JButton rootsButton = new JButton("Roots Button");
+	private ArrayList<String> tokensMisra1 = new ArrayList<>();
+	private JPanel tokenPanel = new JPanel(new FlowLayout(FlowLayout.CENTER));
 
-      
-       
-        JLabel misra1Label = new JLabel("Misra 1:");
-        misra1Field = new JTextField();
-        JLabel misra2Label = new JLabel("Misra 2:");
-        misra2Field = new JTextField();
-     
+	public presentationLayer() {
+		frame = new JFrame("Arabic Poem Database");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setSize(400, 200);
+		frame.setLayout(new GridLayout(5, 2));
 
-        JButton insertButton = new JButton("Insert Verse");
-        insertButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                insertPoem();
-            }
-        });
+		JLabel titleLabel = new JLabel("Title:");
+		titleField = new JTextField();
+		JLabel verseNumberLabel = new JLabel("Verse Number:");
+		verseNumberField = new JTextField();
+		JLabel misra1Label = new JLabel("Misra 1:");
+		misra1Field = new JTextField();
 
+		JButton insertButton = new JButton("Insert Poem");
+		insertButton.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				insertPoem();
+			}
+		});
 
-      
-        frame.add(misra1Label);
-        frame.add(misra1Field);
-        frame.add(misra2Label);
-        frame.add(misra2Field);
-        frame.add(insertButton);
+		JButton TokenButton = new JButton("Tokenize Verses");
+		TokenButton.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				tokenizeVerses();
+			}
+		});
 
-        frame.setVisible(true);
-    }
+		rootsButton.setEnabled(false);
+		
+		frame.add(titleLabel);
+		frame.add(titleField);
+		frame.add(verseNumberLabel);
+		frame.add(verseNumberField);
+		frame.add(misra1Label);
+		frame.add(misra1Field);
 
-    private void insertPoem() {
-      
-      
-        String misra1 = misra1Field.getText();
-        String misra2 = misra2Field.getText();
-      
+		frame.add(insertButton);
+		frame.add(TokenButton);
+		frame.add(tokenPanel);
+		frame.add(rootsButton);
 
-        PoemTO poem = new PoemTO();
-     
-        poem.setMisra1(misra1);
-        poem.setMisra2(misra2);
-     
+		frame.setVisible(true);
+	}
 
-       
+	private void insertPoem() {
+		String title = titleField.getText();
+		int verseNumber = Integer.parseInt(verseNumberField.getText());
+		String misra1 = misra1Field.getText();
 
-        try {
-            PoemDal.savePoem(poem);
-            JOptionPane.showMessageDialog(frame, "Poem inserted successfully.", "Success", JOptionPane.INFORMATION_MESSAGE);
-            clearFields();
-        } catch (SQLException ex) {
-            ex.printStackTrace();
-            JOptionPane.showMessageDialog(frame, "Error occurred while inserting poem.", "Error", JOptionPane.ERROR_MESSAGE);
-        }
-    }
+		PoemTO poem = new PoemTO();
+		poem.setTitle(title);
+		poem.setVerseNumber(verseNumber);
+		poem.setMisra1(misra1);
 
-    private void clearFields() {
-     
-        misra1Field.setText("");
-        misra2Field.setText("");
-    }
+		PoemDal poemDAO = new PoemDal();
 
-    public static void main(String[] args) {
-        SwingUtilities.invokeLater(() -> new presentationLayer());
-    }
+		try {
+			poemDAO.savePoem(poem);
+			JOptionPane.showMessageDialog(frame, "Poem inserted successfully.", "Success",
+					JOptionPane.INFORMATION_MESSAGE);
+			clearFields();
+		} catch (SQLException ex) {
+			ex.printStackTrace();
+			JOptionPane.showMessageDialog(frame, "Error occurred while inserting poem.", "Error",
+					JOptionPane.ERROR_MESSAGE);
+		}
+	}
+
+	private void tokenizeVerses() {
+		String misra1 = misra1Field.getText();
+
+		tokensMisra1.clear(); // Clear previous tokens
+
+		StringTokenizer tokenizer1 = new StringTokenizer(misra1, " ");
+
+		while (tokenizer1.hasMoreTokens()) {
+			String token = tokenizer1.nextToken();
+			tokensMisra1.add(token);
+		}
+
+		refreshTokenButtons();
+		//enableRootsButton();
+	}
+
+	private void refreshTokenButtons() {
+		// Remove existing buttons
+		tokenPanel.removeAll();
+
+		// Create buttons for misra1 tokens
+		for (String token : tokensMisra1) {
+			JButton tokenButton = new JButton(token);
+			tokenButton.addActionListener(new ActionListener() {
+				@Override
+				public void actionPerformed(ActionEvent e) {
+					// Handle button click for token
+					// You can add your custom logic here
+					enableRootsButton();
+				}
+			});
+			tokenPanel.add(tokenButton);
+		}
+
+		frame.revalidate();
+		frame.repaint();
+	}
+
+	 private void enableRootsButton() {
+	        if (tokensMisra1.size() > 0) {
+	            rootsButton.setEnabled(true);
+	        }
+	    }
+	 
+	private void clearFields() {
+		titleField.setText("");
+		verseNumberField.setText("");
+		misra1Field.setText("");
+
+		tokensMisra1.clear();
+
+		tokenPanel.removeAll();
+		frame.revalidate();
+		frame.repaint();
+	}
+
+	public static void main(String[] args) {
+		SwingUtilities.invokeLater(() -> new presentationLayer());
+	}
 }
